9c7f1da8778f3be9619b7956d9990c3aa02356e3
lauk20 <klau20@stuy.edu>
2022-05-16 19:15:03 -0400

    Almost working for C, but colors are wrong...

--- my_main.c
+++ my_main.c
@@ -54,10 +54,10 @@ void my_main() {
   int i;
   struct matrix *tmp;
   struct stack *systems;
-  screen t;
+  screen s;
   zbuffer zb;
-  color g;
-  double step_3d = 20;
+  color c;
+  double step_3d = 100;
   double theta;
 
   //Lighting values here for easy access
@@ -100,16 +100,126 @@ void my_main() {
 
   systems = new_stack();
   tmp = new_matrix(4, 1000);
-  clear_screen( t );
+  clear_screen( s );
   clear_zbuffer(zb);
-  g.red = 0;
-  g.green = 0;
-  g.blue = 0;
+  c.red = 0;
+  c.green = 0;
+  c.blue = 0;
+
+  struct matrix * polygons = new_matrix(4, 1000);
 
   print_symtab();
   for (i=0;i<lastop;i++) {
 
     printf("%d: ",i);
+    switch(op[i].opcode)
+      {
+        case PUSH:
+          push(systems);
+          break;
+        case POP:
+          pop(systems);
+          break;
+        case MOVE:
+          tmp = make_translate(op[i].op.move.d[0], op[i].op.move.d[1], op[i].op.move.d[2]);
+
+          matrix_mult(peek(systems), tmp);
+          copy_matrix(tmp, peek(systems));
+          break;
+        case ROTATE:
+          if (op[i].op.rotate.axis == 0){
+            tmp = make_rotX(op[i].op.rotate.degrees * M_PI/180);
+          } else if (op[i].op.rotate.axis == 1){
+            tmp = make_rotY(op[i].op.rotate.degrees * M_PI/180);
+          } else if (op[i].op.rotate.axis == 2){
+            tmp = make_rotZ(op[i].op.rotate.degrees * M_PI/180);
+          }
+
+          matrix_mult(peek(systems), tmp);
+          copy_matrix(tmp, peek(systems));
+          break;
+        case SCALE:
+          tmp = make_scale(op[i].op.scale.d[0], op[i].op.scale.d[1], op[i].op.scale.d[2]);
+
+          matrix_mult(peek(systems), tmp);
+          copy_matrix(tmp, peek(systems));
+          break;
+        case BOX: ;
+          double x1 = op[i].op.box.d0[0];
+          double y1 = op[i].op.box.d0[1];
+          double z1 = op[i].op.box.d0[2];
+          double x2 = op[i].op.box.d1[0];
+          double y2 = op[i].op.box.d1[1];
+          double z2 = op[i].op.box.d1[2];
+          add_box(polygons, x1, y1, z1, x2, y2, z2);
+          matrix_mult(peek(systems), polygons);
+          if (op[i].op.box.constants != NULL) {
+            if (op[i].op.box.constants->type == SYM_CONSTANTS){
+              draw_polygons(polygons, s, zb, view, light, ambient, op[i].op.box.constants->s.c);
+            }
+          } else {
+            draw_polygons(polygons, s, zb, view, light, ambient, reflect);
+          }
+          polygons->lastcol = 0;
+          break;
+        case SPHERE: ;
+          x1 = op[i].op.sphere.d[0];
+          y1 = op[i].op.sphere.d[1];
+          z1 = op[i].op.sphere.d[2];
+          double r = op[i].op.sphere.r;
+          add_sphere(polygons, x1, y1, z1, r, step_3d);
+          matrix_mult(peek(systems), polygons);
+          if (op[i].op.sphere.constants != NULL){
+            if (op[i].op.sphere.constants->type == SYM_CONSTANTS){
+              printf("constants: %f %f %f\n", op[i].op.sphere.constants->s.c->r[0], op[i].op.sphere.constants->s.c->r[1], op[i].op.sphere.constants->s.c->r[2]);
+              draw_polygons(polygons, s, zb, view, light, ambient, op[i].op.sphere.constants->s.c);
+            }
+          } else {
+            draw_polygons(polygons, s, zb, view, light, ambient, reflect);
+          }
+          polygons->lastcol = 0;
+          break;
+        case TORUS: ;
+          x1 = op[i].op.torus.d[0];
+          y1 = op[i].op.torus.d[1];
+          z1 = op[i].op.torus.d[2];
+          double r0 = op[i].op.torus.r0;
+          double r1 = op[i].op.torus.r1;
+          add_torus(polygons, x1, y1, z1, r0, r1, step_3d);
+          matrix_mult(peek(systems), polygons);
+          if (op[i].op.torus.constants != NULL){
+            if (op[i].op.torus.constants->type == SYM_CONSTANTS){
+              draw_polygons(polygons, s, zb, view, light, ambient, op[i].op.torus.constants->s.c);
+            }
+          } else {
+            draw_polygons(polygons, s, zb, view, light, ambient, reflect);
+          }
+          polygons->lastcol = 0;
+          break;
+        case CONSTANTS: ;
+          break;
+        case LINE: ;
+          x1 = op[i].op.line.p0[0];
+          y1 = op[i].op.line.p0[1];
+          z1 = op[i].op.line.p0[1];
+          x2 = op[i].op.line.p1[0];
+          y2 = op[i].op.line.p1[1];
+          z2 = op[i].op.line.p1[1];
+          if (op[i].op.line.constants != NULL){
+            if (op[i].op.line.constants->type == SYM_CONSTANTS){
+              draw_line(x1, y1, z1, x2, y2, z2, s, zb, c);
+            }
+          } else {
+            draw_line(x1, y1, z1, x2, y2, z2, s, zb, c);
+          }
+          break;
+        case SAVE: ;
+          save_extension(s, op[i].op.save.p->name);
+          break;
+        case DISPLAY: ;
+          display(s);
+          break;
+      }
 
     printf("\n");
   }
